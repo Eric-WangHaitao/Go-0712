c83332257f05a61d74b3995a2a0c2893af66fd73
haitao <wanghaitao2@oppo.com>
2021-08-08 19:49:43 +0800

    Week04 homework

--- Week04/internal/dao/user.go
+++ Week04/internal/dao/user.go
@@ -11,23 +11,19 @@ type UserRepository interface {
 	AddUser()
 }
 
-// UserRepository接口实现
 type userRepo struct {
 	*sql.DB
 }
 
-// 新增user
 func (u *userRepo) AddUser() {
 	user := &model.User{}
 	user.Id = 1
-	user.Name = "小明"
+	user.Name = "xiaoming"
 	log.Println("add user :" + user.Name)
 }
 
-// 根据*sql.DB初始化 *userRepo
 func NewUserRepo(db *sql.DB) *userRepo {
 	return &userRepo{}
 }
 
-// 声明NewUserRepo的返回值是UserRepository接口类型
 var UserSet = wire.NewSet(NewUserRepo, wire.Bind(new(UserRepository), new(*userRepo)))
